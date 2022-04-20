8c0319eba959a260d7421c6e624896c15cc1facc
Aikanysh <lailievaa02@gmail.com>
2022-04-20 20:36:05 +0600

    updated

--- src/car/car.controller.ts
+++ src/car/car.controller.ts
@@ -1,5 +1,10 @@
-import { Body, Controller, Delete, Get, Param, Post, Put } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Post, Put, UseGuards } from '@nestjs/common';
 import { ApiConflictResponse, ApiCreatedResponse, ApiNotFoundResponse } from '@nestjs/swagger';
+import { JwtGuard } from 'src/auth/guards/jwt.guard';
+import { RolesGuard } from 'src/auth/guards/roles.guard';
+import { Roles } from 'src/auth/roles.decorator';
+
+import { Role } from 'src/user/entity/role.enum';
 import { CarService } from './car.service';
 import { CreateCarDto, UpdateCarDto } from './dto';
 
@@ -11,28 +16,40 @@ export class CarController {
         return this.service.findAll();
     }
     
+
     @Get(':carId')
     findOne(@Param('carId') carId:number) {
         return this.service.findCarId(carId);
     }
 
+    @Roles(Role.ADMIN)
+    @UseGuards(JwtGuard,RolesGuard)
     @Post()
     @ApiCreatedResponse({ type: CreateCarDto})
     @ApiConflictResponse({ description: 'already exist' })
     create(@Body() dto:CreateCarDto){
-    return this.service.create(dto);
+        return this.service.create(dto);
     }
+
+    @Roles(Role.ADMIN)
+    @UseGuards(JwtGuard,RolesGuard)
     @Put(':carId')
     @ApiCreatedResponse({ type: UpdateCarDto })
     @ApiNotFoundResponse()
     async update(@Param('carId') carId: number,  @Body() dto: UpdateCarDto) {
       return await this.service.update(carId, dto);
     }
+
+    @Roles(Role.ADMIN)
+    @UseGuards(JwtGuard,RolesGuard)
     @Delete(':carId')
     @ApiNotFoundResponse({ description: ' already deleted' })
     async delete(@Param('carId') carId: number) {
         return this.service.delete(carId);
     }
+
+    @Roles(Role.ADMIN)
+    @UseGuards(JwtGuard,RolesGuard)
     @Delete()
     @ApiNotFoundResponse({ description: ' already deleted' })
     async deleteAll() {
--- src/car/car.module.ts
+++ src/car/car.module.ts
@@ -3,10 +3,12 @@ import { CarService } from './car.service';
 import { CarController } from './car.controller';
 import { CarEntity } from './entity';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { JwtGuard } from 'src/auth/guards/jwt.guard';
+import { RolesGuard } from 'src/auth/guards/roles.guard';
 
 @Module({
   imports:[TypeOrmModule.forFeature([CarEntity])],
-  providers: [CarService],
+  providers: [CarService,RolesGuard,JwtGuard],
   controllers: [CarController],
   exports:[CarService]
 })
