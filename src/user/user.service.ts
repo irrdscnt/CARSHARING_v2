6cc85bc45fb9a3791366fd23938cd7540c054ce2
Aikanysh <lailievaa02@gmail.com>
2022-04-20 20:40:43 +0600

    added methods for user

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,4 +1,38 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { from } from 'rxjs';
+import { Repository } from 'typeorm';
+import { CreateUserDto } from './dto';
+import { UserEntity } from './entity';
+import { User } from './interface';
 
 @Injectable()
-export class UserService {}
+export class UserService {
+    constructor (@InjectRepository(UserEntity)
+    private readonly regRep:Repository<UserEntity>
+    ){}
+    async findPass(password:string){
+        return from (this.regRep.find({password}))
+
+    }
+
+    async findByEmail(email:string){
+        return from (this.regRep.find({email}))
+    }
+
+    async save(dto:CreateUserDto){
+        return from (this.regRep.save(dto))
+    }
+
+    async findUserId(id: number) {
+        const userid = await this.regRep.findOne({id})
+        console.log(userid)
+        if (!userid) {
+            throw new BadRequestException(400,'This user ID does not exist!');
+        }
+        return userid
+        //else{
+        //     return userid
+        // }
+    }
+}
